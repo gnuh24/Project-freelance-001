029b2a3c11d8310098b107f23e01ca38b53d906a
huybang2017 <ndhbang2017@gmail.com>
2024-06-20 00:22:08 +0700

    feat: fix config routes

--- FrontEnd/src/Routes.jsx
+++ FrontEnd/src/Routes.jsx
@@ -1,27 +1,15 @@
-import { Suspense, lazy } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import React, { lazy } from 'react'
 
 // Lazy load components
-const Home = lazy(() => import('./layouts/Home.jsx'))
-
-const Routes = () => {
-  return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto p-4">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route path="/" exact component={Home} />
-            </Switch>
-          </Suspense>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  )
+const Home = lazy(() => import('./layouts/Home'))
+const SignIn = lazy(() => import('./layouts/SignIn'))
+const SignUp = lazy(() => import('./layouts/SignUp'))
+const ErrorPage = lazy(() => import('./layouts/ErrorPage'))
+const Routes = {
+  home: { path: '/', element: <Home /> },
+  signIn: { path: '/signIn', element: <SignIn /> },
+  signUp: { path: '/signUp', element: <SignUp /> },
+  errorPage: { path: '*', element: <ErrorPage /> },
 }
 
 export default Routes
