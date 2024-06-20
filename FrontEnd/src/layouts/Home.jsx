92c377db6ce6606f1665b98746daeb44018d46d4
huybang2017 <ndhbang2017@gmail.com>
2024-06-20 20:24:37 +0700

    feat: add component into page home

--- FrontEnd/src/layouts/Home.jsx
+++ FrontEnd/src/layouts/Home.jsx
@@ -1,5 +1,17 @@
+import { Carousel } from 'flowbite-react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Products from '../components/Products'
+
 const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <>
+      <Header></Header>
+      <Carousel></Carousel>
+      <Products></Products>
+      <Footer></Footer>
+    </>
+  )
 }
 
 export default Home
