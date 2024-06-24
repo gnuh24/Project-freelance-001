312816fb1830ce991a7c1e0702984950fd34594d
huybang2017 <ndhbang2017@gmail.com>
2024-06-25 01:06:20 +0700

    fix: update path import

--- FrontEnd/src/layouts/Home.jsx
+++ FrontEnd/src/layouts/Home.jsx
@@ -1,12 +1,13 @@
-import { Carousel } from 'flowbite-react'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import Products from '../components/Products'
-
+import Header from '../components/ingredient/Header.jsx'
+import Footer from '../components/ingredient/Footer'
+import Carousel from '../components/home/Carousel'
+import Products from '../components/home/Products'
+import Navigation from '../components/home/Navigation'
 const Home = () => {
   return (
     <>
       <Header></Header>
+      <Navigation></Navigation>
       <Carousel></Carousel>
       <Products></Products>
       <Footer></Footer>
