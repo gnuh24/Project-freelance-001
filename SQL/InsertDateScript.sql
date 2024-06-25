f2e61a178bbc7095a5a04e77129dc5d581bde37b
THug24 <devilskipper@gmail.com>
2024-06-26 02:49:31 +0700

    Script SQL Shoe Type & Brand

---
+++ SQL/InsertDateScript.sql
@@ -0,0 +1,12 @@
+USE `Project_Freelance_001`;
+
+INSERT INTO `ShoeType`	(`ShoeTypeId`, 		`ShoeTypeName`)
+				VALUES  (1, 				"Các loại giày khác",
+						 2, 				"Giày bata",
+						 3, 				"Giày Sandal");
+                         
+INSERT INTO `Brand`		(`BrandId`, 		`BrandName`, 				`Logo`)
+				VALUES  (1, 				"Các thương hiệu khác",		"Hình 1",
+						 2, 				"Adidas",					"Hình 2",
+						 3, 				"Nike"						"Hình 3")                         
+                         
