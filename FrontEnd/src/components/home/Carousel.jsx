d362a58e7906a9a879bd3283252a774dd3689305
huybang2017 <ndhbang2017@gmail.com>
2024-06-27 17:29:03 +0700

    feat: add carousel

--- FrontEnd/src/components/home/Carousel.jsx
+++ FrontEnd/src/components/home/Carousel.jsx
@@ -1,7 +1,29 @@
+import { Carousel } from 'flowbite-react'
+
 const Carusel = () => {
   return (
     <>
-      <h1>carousel</h1>
+      <div className="h-9/10-screen pt-3">
+        <Carousel slideInterval={2000}>
+          <img src="../../../public/image/banner.png" alt="..." />
+          <img
+            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+            alt="..."
+          />
+          <img
+            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+            alt="..."
+          />
+          <img
+            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+            alt="..."
+          />
+          <img
+            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+            alt="..."
+          />
+        </Carousel>
+      </div>
     </>
   )
 }
