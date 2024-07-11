222948e0537ddffcedb9bf3f1e5825186e963d90
huybang2017 <ndhbang2017@gmail.com>
2024-07-12 02:19:28 +0700

    feat: add base layout

--- FrontEnd/src/layouts/user/BaseLayoutUser.jsx
+++ FrontEnd/src/layouts/user/BaseLayoutUser.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import Header from '../../components/ingredient/Header'
+import Navigation from '../../components/ingredient/Navigation'
+import FooterComponent from '../../components/ingredient/Footer'
 
 const BaseLayoutUser = () => {
   return (
     <>
+      <Header />
+      <Navigation />
       <Outlet />
+      <FooterComponent />
     </>
   )
 }
