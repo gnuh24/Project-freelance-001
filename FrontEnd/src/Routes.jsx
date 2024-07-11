bb824b2a696d3633d9140ff49221c2546b9b93df
huybang2017 <ndhbang2017@gmail.com>
2024-07-12 02:19:01 +0700

    feat: config file

--- FrontEnd/src/Routes.jsx
+++ FrontEnd/src/Routes.jsx
@@ -1,16 +1,19 @@
 import { lazy } from 'react'
-import BaseLayoutUser from './layouts/user/BaseLayoutUser.jsx'
-import BaseLayoutDashBoard from './layouts/dashboard/BaseLayoutDashboard.jsx'
 import { createBrowserRouter } from 'react-router-dom'
 
-// Lazy load components
 const Home = lazy(() => import('./layouts/user/Home'))
 const SignIn = lazy(() => import('./layouts/auth/SignIn'))
 const SignUp = lazy(() => import('./layouts/auth/SignUp'))
 const ErrorPage = lazy(() => import('./layouts/ErrorPage'))
 const DetailProduct = lazy(() => import('./layouts/user/DetailProduct.jsx'))
 const DashBoard = lazy(() => import('./layouts/dashboard/Dashboard.jsx'))
-
+const Checkout = lazy(() => import('./layouts/user/CheckOut.jsx'))
+const ProductsDashBoard = lazy(() => import('./layouts/dashboard/Products.jsx'))
+const BaseLayoutUser = lazy(() => import('./layouts/user/BaseLayoutUser.jsx'))
+const BaseLayoutDashBoard = lazy(
+  () => import('./layouts/dashboard/BaseLayoutDashboard.jsx'),
+)
+const ProfileDashBoard = lazy(() => import('./layouts/dashboard/Profile.jsx'))
 const router = createBrowserRouter([
   {
     path: '/',
@@ -20,12 +23,21 @@ const router = createBrowserRouter([
       { path: '/detailProduct/:id', element: <DetailProduct /> },
       { path: '/signIn', element: <SignIn /> },
       { path: '/signUp', element: <SignUp /> },
+      { path: '/checkout', element: <Checkout /> },
     ],
   },
   {
     path: '/dashboard',
     element: <BaseLayoutDashBoard />,
-    children: [{ element: <DashBoard />, index: true }],
+    children: [
+      { element: <DashBoard />, index: true }, // Change path to '/' for dashboard
+      { path: '/dashboard/products', element: <ProductsDashBoard /> },
+      { path: '/dashboard/profile', element: <ProfileDashBoard /> },
+    ],
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
   },
 ])
 
