aefd43e5c512a81d2d7e2ea199c9349daf0746d5
huybang2017 <ndhbang2017@gmail.com>
2024-08-16 14:34:33 +0700

    feat: add api update information account

--- FrontEnd/src/apis/user/Account.jsx
+++ FrontEnd/src/apis/user/Account.jsx
@@ -1,5 +1,5 @@
-import AxiosAdmin from '../AxiosAdmin';
-import axios from 'axios';  // Make sure axios is imported
+import AxiosAdmin from '../AxiosAdmin'
+import axios from 'axios' // Make sure axios is imported
 
 // Get users without pagination
 
@@ -32,52 +32,54 @@ import axios from 'axios';  // Make sure axios is imported
 
 // Get users with pagination, sorting, and search
 const getAccountsAPI = async (pageSize, pageNumber, sort, search) => {
-    const params = {};
-    if (pageSize !== null && pageSize !== undefined) params.pageSize = pageSize;
-    if (pageNumber !== null && pageNumber !== undefined) params.pageNumber = pageNumber;
-    if (sort !== null && sort !== undefined) params.sort = sort;
-    if (search !== null && search !== undefined) params.search = search;
+  const params = {}
+  if (pageSize !== null && pageSize !== undefined) params.pageSize = pageSize
+  if (pageNumber !== null && pageNumber !== undefined)
+    params.pageNumber = pageNumber
+  if (sort !== null && sort !== undefined) params.sort = sort
+  if (search !== null && search !== undefined) params.search = search
 
-    try {
-        const data = await AxiosAdmin.get('/Account', { params });
-        return data;
-    } catch (error) {
-        console.error('Error fetching users:', error);
-        throw error;
-    }
-};
+  try {
+    const data = await AxiosAdmin.get('/Account', { params })
+    return data
+  } catch (error) {
+    console.error('Error fetching users:', error)
+    throw error
+  }
+}
 
 // Get a single user by ID
-const getAccountAndUserInformationById = async (id) => {
-    try {
-        const data = await AxiosAdmin.get(`/Account/${id}`);
-        return data;
-    } catch (error) {
-        console.error('Error fetching user:', error);
-        throw error;
-    }
-};
+const getAccountAndUserInformationByIdAPI = async (id) => {
+  const data = await AxiosAdmin.get(`/Account/${id}`)
+  return data
+}
+
+const updateAccountInformationUserAPI = async (payload) => {
+  const data = await AxiosAdmin.patch('/Account/UpdateInformation', payload)
+  return data
+}
 
 // Update an existing user
 const putAccountAPI = async (account) => {
   try {
-      // Create a FormData object
-      const formData = new FormData();
-      formData.append('accountId', account.accountId);
-      formData.append('status', account.status);
+    // Create a FormData object
+    const formData = new FormData()
+    formData.append('accountId', account.accountId)
+    formData.append('status', account.status)
 
-      // Make the PUT request with form-data
-      const response = await AxiosAdmin.patch('/Account/ChangeStatus', formData);
+    // Make the PUT request with form-data
+    const response = await AxiosAdmin.patch('/Account/ChangeStatus', formData)
 
-      return response;  // Ensure response data is returned
+    return response // Ensure response data is returned
   } catch (error) {
-      console.error('Error updating account status:', error);
-      throw new Error('Error updating account status');
+    console.error('Error updating account status:', error)
+    throw new Error('Error updating account status')
   }
-};
+}
 
 export {
-    getAccountsAPI,
-    getAccountAndUserInformationById,
-    putAccountAPI,
-};
+  getAccountsAPI,
+  getAccountAndUserInformationByIdAPI,
+  putAccountAPI,
+  updateAccountInformationUserAPI,
+}
