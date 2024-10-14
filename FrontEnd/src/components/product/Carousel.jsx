d79741d8c8dfcc3c9a4a0c4a86cb4a7a68b22f04
huybang2017 <ndhbang2017@gmail.com>
2024-10-14 15:58:30 +0700

    .

--- FrontEnd/src/components/product/Carousel.jsx
+++ FrontEnd/src/components/product/Carousel.jsx
@@ -1,27 +1,30 @@
 import { Carousel } from 'flowbite-react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCurrentEvent } from '../../reducers/eventReducer/EventSlice'
+import Loader from '../loader/Loader'
 
 const Carusel = () => {
+  const dispatch = useDispatch()
+  const { data, status, err } = useSelector((state) => state.events)
+
+  useEffect(() => {
+    dispatch(getCurrentEvent())
+  }, [dispatch])
+  console.log(data)
   return (
     <>
       <div className="h-9/10-screen pt-3">
         <Carousel slideInterval={2000}>
           <img src="../../../public/image/banner.png" alt="..." />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-            alt="..."
-          />
+          {data && Array.isArray(data) && data.length > 0 && data[0].banner ? (
+            <img
+              src={`http://localhost:8080/Event/Banner/${data[0].banner}`}
+              alt={data[0].title || 'Event Banner'}
+            />
+          ) : (
+            <img src="../../../public/image/banner.png" alt="..." />
+          )}
         </Carousel>
       </div>
     </>
