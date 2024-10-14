2ec920950a1e4f939f3c06d27e88765bf0641bbc
THug24 <devilskipper@gmail.com>
2024-10-14 19:37:45 +0700

    FE Admin Header: Loại bỏ các Giao diện thừa

--- FrontEnd/src/components/admin/Header.jsx
+++ FrontEnd/src/components/admin/Header.jsx
@@ -26,7 +26,7 @@ export function HeaderDashboard() {
           alt="Flowbite React Logo"
         />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite React
+          Big Boys Sneaker Big Size
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
@@ -37,21 +37,11 @@ export function HeaderDashboard() {
           label={
             <Avatar
               alt="User settings"
-              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              img="../../../public/image/logo/LOGO.png"
               rounded
             />
           }
         >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">
-              name@flowbite.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
           <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
         </Dropdown>
         <Navbar.Toggle />
