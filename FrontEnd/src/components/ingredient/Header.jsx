105a924a670683ed3dd5c9afd66a545ba168066d
huybang2017 <ndhbang2017@gmail.com>
2024-10-24 10:12:56 +0700

    fix: fix bug

--- FrontEnd/src/components/ingredient/Header.jsx
+++ FrontEnd/src/components/ingredient/Header.jsx
@@ -89,7 +89,7 @@ const Header = () => {
                     </li>
                     <li>
                       <Link
-                        to="/listOrderByUser"
+                        to="/orders"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         Danh sách đơn hàng
